d22c453c9a4dccf4944e8a2bb679ffa1e863d12b
Yevgen <107062952+Yevgen1984@users.noreply.github.com>
2022-08-30 21:36:54 +0300

    add HW7

---
+++ js/01-gallery.js
@@ -0,0 +1,34 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const list = document.querySelector(".gallery");
+const newImg = galleryItems
+  .map(
+    (image) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+        data-source="${image.original}"                      
+      alt="${image.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+list.insertAdjacentHTML("beforeend", newImg);
+list.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <img
+      class="gallery__image"
+      src="${e.target.dataset.source}"
+    />
+`);
+
+  instance.show();
+});
+console.log(galleryItems);
+
